082bb1b7235c5dade10a73eb496db81e6e5296b9
Guilherme Hashioka <guilherme.hashioka@gmail.com>
2020-08-05 15:21:30 -0300

    feat: 🎸 set iddentification and password on login modal

--- js/src/forum/components/LoginPage.js
+++ js/src/forum/components/LoginPage.js
@@ -1,8 +1,19 @@
-import RedirectToHomeAndOpenModalPage from './RedirectToHomeAndOpenModalPage';
-import LogInModal from 'flarum/components/LogInModal';
+import RedirectToHomeAndOpenModalPage from "./RedirectToHomeAndOpenModalPage";
+import LogInModal from "flarum/components/LogInModal";
 
 export default class LoginPage extends RedirectToHomeAndOpenModalPage {
+    init() {
+        const params = m.route.param();
+        this.identification = params.identification;
+        this.password = params.password;
+
+        super.init();
+    }
+
     createModal() {
-        return new LogInModal();
+        return new LogInModal({
+          identification: this.identification,
+          password: this.password
+        });
     }
 }
